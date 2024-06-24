88bcdbc13f31a7e73444d2e5dc82afa8c1de7a6a
srcrapi <rafa1000.skylanders@gmail.com>
2024-06-24 15:42:59 +0100

    chore: Add a way to view the img URL

--- app.py
+++ app.py
@@ -4,21 +4,16 @@ import json
 import tkinter as tk
 import pandas as pd
 from pandas.io.parsers import TextFileReader
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions 
 from tkinter import filedialog
 from flask import Flask, render_template, jsonify
 from markupsafe import escape
 from werkzeug.serving import make_server
+from components.web_searcher import WebSearcher
 import webview
 
 
 app = Flask(__name__)
+web_search = WebSearcher()
 
 @app.route("/")
 def index():
@@ -34,50 +29,9 @@ def select_csv_file():
     root = tk.Tk()
     root.withdraw()
 
-    file_path = filedialog.askopenfilename(initialdir="~", title="Select a file")
+    file_path = filedialog.askopenfilename(initialdir="~", title="Select a file", filetypes=[("CSV files", "*.csv")])
 
     return file_path
-
-
-"""
-def web_search(search_term: str, num_images: int = 6) -> list:
-    Pesquisa o item no google images e retorna a sua url
-
-    :param search_term: Termo que vai ser usado na pesquisa
-    :return: Retorna a url da imagem
-
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-
-    try:
-        driver.get("https://www.google.com/imghp")
-
-        search_box = WebDriverWait(drive, 10).until(
-            EC.presence_of_element_located((By.NAME, "q"))
-        )
-        search_box.send_keys(search_term)
-        search_box.send_keys(Keys.RETURN)
-
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'img.rg_i'))
-        )
-
-        drive.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'img.rg_i'))
-        )
-
-        images = driver.find_element_by_css_selector("img.rg_i")
-
-        url = [img.get_attribute("src") for img in imagens[:num_images]]
-
-        return url
-    finally:
-        driver.quit()
-"""
 
 
 def save_data(data, filename: str) -> None:
@@ -97,21 +51,22 @@ def get_data():
 
         df: TextFileReader = pd.read_csv(csv_file_path, delimiter=";")
     
-        marcas = df["Marca"].tolist()
-        referencia = df["Ref"].tolist()
-        
         data = df.to_dict(orient="records")
 
         save_data(data, "data.json")
-        """
-        urls = []
+
+        urls = {}
 
         for item in data:
-            url_images: list = web_search(f"{item['Marca']} {item['Ref']}")
-            print(url_images)
-            urls.append({item["Ref"]: url_images})
-        """
+            url_images = web_search.search(f"{item['Marca']} {item['Ref']}")
+            urls[item["Ref"]] = url_images
+                        
 
+        for ref, image in urls.items(): 
+            print(f"Referencia {ref}:")
+            for img in image:
+                print(f"Imagens: {img}")
+            
         return render_template("index.html", data=data)
 
     except Exception as e:
